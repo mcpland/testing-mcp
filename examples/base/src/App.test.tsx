4403f85693ab1b534c24ce3a7d20b0ffbca2227c
unadlib <unadlib@gmail.com>
2025-10-31 02:37:55 +0800

    chore(example): update

--- examples/base/src/App.test.tsx
+++ examples/base/src/App.test.tsx
@@ -1,17 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { connect } from "../../../src/client";
 import { describe, it, expect } from "vitest";
 
 import App from "./App";
 
 describe("App", () => {
-  it(
-    "increments the counter when the button is clicked",
-    async () => {
-      render(<App />);
-      await connect({ port: 3001, filePath: __filename });
-    },
-    1000 * 60 * 10
-  );
+  it("increments the counter when the button is clicked", async () => {
+    render(<App />);
+  });
 });
