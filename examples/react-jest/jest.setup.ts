90a023c6c7c98ad0baa49e554a66706e2c62f5a7
unadlib <unadlib@gmail.com>
2025-11-02 22:21:08 +0800

    chore(example): update

--- examples/react-jest/jest.setup.ts
+++ examples/react-jest/jest.setup.ts
@@ -1,1 +1,23 @@
-import '@testing-library/jest-dom'
+import "@testing-library/jest-dom";
+import { screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { connect } from "../../src/client";
+const timeout = 10 * 60 * 1000;
+
+if (process.env.TESTING_MCP) {
+  jest.setTimeout(timeout);
+}
+
+afterEach(async () => {
+  if (!process.env.TESTING_MCP) return;
+  const state = expect.getState();
+  await connect({
+    port: 3001,
+    filePath: state.testPath,
+    context: {
+      userEvent,
+      screen,
+      fireEvent,
+    },
+  });
+}, timeout);
