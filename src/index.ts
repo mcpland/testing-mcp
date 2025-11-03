cd17caa2f5cf02284ec1ead129ba4e305817fb6e
unadlib <unadlib@gmail.com>
2025-11-04 02:54:46 +0800

    chore(version): update version to 0.3.0

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ class TestingMCPServer {
     this.server = new Server(
       {
         name: "testing-mcp",
-        version: "0.4.0",
+        version: "0.3.0",
       },
       {
         capabilities: {
