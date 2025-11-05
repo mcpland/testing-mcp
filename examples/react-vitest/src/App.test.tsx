df961acf03201cdc6aa12b850892bacb8dfc6316
unadlib <unadlib@gmail.com>
2025-11-06 00:36:21 +0800

    chore(example): update

--- examples/react-vitest/src/App.test.tsx
+++ examples/react-vitest/src/App.test.tsx
@@ -7,10 +7,5 @@ import App from "./App";
 describe("App", () => {
   it("increments the counter when the button is clicked", async () => {
     render(<App />);
-    const button = screen.getByRole("button", { name: /count is 0/i });
-    await userEvent.click(button);
-    expect(
-      screen.getByRole("button", { name: /count is 1/i })
-    ).toBeInTheDocument();
   });
 });
